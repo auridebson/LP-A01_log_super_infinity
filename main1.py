2d04ab0b2ffa9635c4f1b6ef6c3d6f6b3447fe6c
auridebson <auridebson@vutus.com>
2023-11-24 11:20:32 -0300

    Update

---
+++ main1.py
@@ -0,0 +1,17 @@
+import random
+
+def ln(x):
+    print("-"*x)
+
+familia = [
+    "auridebson",
+    "luciana",
+    "levy",
+    "guilherme"
+]
+
+ln(30)
+familia.pop(-2)
+for membro in familia:
+    print(membro)
+ln(30)
