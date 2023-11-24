a1d2c844a094093fe43d0fd8fb6384d5e1b1a4c2
auridebson <auridebson@vutus.com>
2023-11-24 12:19:22 -0300

    Update

--- main1.py
+++ main1.py
@@ -1,5 +1,8 @@
 import random
 
+def ln(x):
+    print("-"*x)
+
 serasa = [
     ["auridebson",2],
     ["luciana",5],
@@ -16,8 +19,27 @@ paises = (
     "Estados Unidos da América"
 )
 
+carrinho = [
+    ("maça",12.4),
+    ("banana",3.4),
+    ("pera",15.7),
+    ("goiaba",4.5),
+]
+
+# 1
 # for divida in serasa:
 #     print(divida[1]**2)
 
-for pais in paises:
-    print(f"O país é {pais} e tem {len(pais)} letras.")
+# 2
+# for pais in paises:
+#     print(f"O país é {pais} e tem {len(pais)} letras.")
+
+# 3
+# total_carrinho = 0
+
+for item in carrinho:
+    total_carrinho += item[1]
+
+ln(40)
+print(f"O valor total do carrinho é R$ {total_carrinho}")
+ln(40)
